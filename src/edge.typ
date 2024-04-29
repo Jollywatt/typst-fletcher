Jollywatt/typst-fletcher

commit 13b452bebee609bc7ae2683fd748b2ca83220053

feat: use state to allow users to change marks

src/edge.typ
@@ -56,7 +56,6 @@
 		resolve-mark(mark, defaults: (
 			pos: i/calc.max(1, marks.len() - 1),
 			rev: i == 0,
-			// rev: false,
 		))
 	}).filter(mark => mark != none) // drop empty marks
 
@@ -97,7 +96,7 @@
 	assert(type(arg) == str)
 	let text = arg
 
-	let mark-names = MARKS.keys().sorted(key: i => -i.len())
+	let mark-names = MARKS.get().keys().sorted(key: i => -i.len())
 	let LINES = LINE_ALIASES.keys().sorted(key: i => -i.len())
 
 	let eat(arg, options) = {
@@ -464,7 +463,7 @@
 ///   - A shorthand string such as `"->"` or `"hook'-/->>"`. Specifically,
 ///     shorthand strings are of the form $M_1 L M_2$ or $M_1 L M_2 L M_3$,
 ///     where
-///     $ M_i in #`fletcher.MARKS` = {#fletcher.MARKS.keys().slice(10).map(raw).join($,$), ...} $
+///     $ M_i in #`fletcher.MARKS` = {#context fletcher.MARKS.get().keys().slice(10).map(raw).join($,$), ...} $
 ///     is a mark name and
 ///     $ L in #`fletcher.LINE_ALIASES` = {#fletcher.LINE_ALIASES.keys().map(raw.with(lang: none)).join($,$)} $
 ///     is the line style.
@@ -703,7 +702,7 @@
 	)
 
 	options += interpret-edge-args(args, options)
-	options += interpret-marks-arg(options.marks)
+	// options += interpret-marks-arg(options.marks)
 
 	// relative coordinate shorthands
 	let interpret-coord-str(coord) = {
@@ -737,23 +736,6 @@
 
 	if type(options.shift) != array { options.shift = (options.shift, options.shift) }
 
-	if type(options.decorations) == str {
-		options.decorations = (
-			"wave": cetz.decorations.wave.with(
-				amplitude: .12,
-				segment-length: .2,
-			),
-			"zigzag": cetz.decorations.zigzag.with(
-				amplitude: .12,
-				segment-length: .2,
-			),
-			"coil": cetz.decorations.coil.with(
-				amplitude: .15,
-				segment-length: .15,
-				factor: 140%,
-			),
-		).at(options.decorations)
-	}
 
 	let obj = (
 		class: "edge",
@@ -776,6 +758,8 @@
 
 #let resolve-edge-options(edge, options) = {
 	let to-pt(len) = to-abs-length(len, options.em-size)
+
+	edge += interpret-marks-arg(edge.marks)
 
 	edge.stroke = pass-none(stroke)(edge.stroke)
 
@@ -801,6 +785,24 @@
 		if type(d) == length { to-pt(d) }
 		else { d*edge.stroke.thickness }
 	})
+
+	if type(edge.decorations) == str {
+		edge.decorations = (
+			"wave": cetz.decorations.wave.with(
+				amplitude: .12,
+				segment-length: .2,
+			),
+			"zigzag": cetz.decorations.zigzag.with(
+				amplitude: .12,
+				segment-length: .2,
+			),
+			"coil": cetz.decorations.coil.with(
+				amplitude: .15,
+				segment-length: .15,
+				factor: 140%,
+			),
+		).at(edge.decorations)
+	}
 
 	edge.crossing-fill = map-auto(edge.crossing-fill, options.crossing-fill)
 	edge.crossing-thickness = map-auto(edge.crossing-thickness, options.crossing-thickness)

docs/manual.typ
@@ -745,7 +745,7 @@ A few other marks are supported, and can be placed anywhere along the edge.
 
 All the built-in marks are defined in `fletcher.MARKS`:
 
-#{fletcher.MARKS.keys()
+#{context fletcher.MARKS.get().keys()
 	.sorted(key: i => i.len())
 	.map(raw.with(lang: "none"))
 	.join[, ]
@@ -917,7 +917,7 @@ You can add any properties to a mark dictionary, but there are some with special
 
 The last few properties control the fine behaviours of how marks connect to the target point and to the edge's stroke.
 
-#grid(
+#context grid(
 	columns: (1fr, 1fr),
 	align: center + horizon,
 	fletcher.mark-debug((inherit: "}>", fill: none), show-offsets: false),
@@ -1060,7 +1060,8 @@ You can create incrementally-revealed diagrams in Touying presentation slides by
 
 == `marks.typ`
 
-The default marks are defined in the `fletcher.MARKS` dictionary with keys: #fletcher.MARKS.keys().map(raw).join(last: [, and ])[, ].
+The default marks are defined in the `fletcher.MARKS` dictionary with keys:
+#context fletcher.MARKS.get().keys().map(raw).join(last: [, and ])[, ].
 
 #show-fns("/src/marks.typ", level: 2, outline: true)
 

src/default-marks.typ
@@ -1,7 +1,7 @@
 #import "deps.typ"
 #import deps.cetz.draw
 
-#let MARKS = (
+#let DEFAULT_MARKS = (
 	// all numbers are interpreted as multiples of stroke thickness
 
 	head: (
@@ -235,3 +235,5 @@
 	"*": (inherit: "circle", fill: auto),
 	"@": (inherit: "circle", size: 4, fill: auto),
 )
+
+#let MARKS = state("fletcher-marks", DEFAULT_MARKS)

src/main.typ
@@ -608,8 +608,10 @@
 	},
 ) = {
 
-	if type(spacing) != array { spacing = (spacing, spacing) }
-	if type(cell-size) != array { cell-size = (cell-size, cell-size) }
+	let spacing = as-pair(spacing)
+	let cell-size = as-pair(cell-size)
+	// if type(spacing) != array { spacing = (spacing, spacing) }
+	// if type(cell-size) != array { cell-size = (cell-size, cell-size) }
 
 	let options = (
 		debug: int(debug),
@@ -635,7 +637,7 @@
 	box(style(styles => {
 		let options = options
 
-		options.em-size = measure(h(1em), styles).width
+		options.em-size = measure(h(1em)).width
 		let to-pt(len) = to-abs-length(len, options.em-size)
 		options.spacing = options.spacing.map(to-pt)
 		options.cell-size = options.cell-size.map(to-pt)

src/marks.typ
@@ -54,6 +54,7 @@
 
 
 #let apply-mark-inheritances(mark) = {
+	let marks = MARKS.get()
 	while "inherit" in mark {
 
 		if mark.inherit.at(-1) == "'" {
@@ -61,9 +62,9 @@
 			mark.inherit = mark.inherit.slice(0, -1)
 		}
 
-		assert(mark.inherit in MARKS, message: "Mark style " + repr(mark.inherit) + " not defined.")
+		assert(mark.inherit in marks, message: "Mark style " + repr(mark.inherit) + " not defined.")
 
-		let parent = MARKS.at(mark.remove("inherit"))
+		let parent = marks.at(mark.remove("inherit"))
 		mark = parent + mark
 	}
 	mark
@@ -74,7 +75,7 @@
 /// Resolve a mark dictionary by applying inheritance, adding any required entries, and evaluating any closure entries.
 ///
 /// #example(```
-/// fletcher.resolve-mark((
+/// context fletcher.resolve-mark((
 /// 	a: 1,
 /// 	b: 2,
 /// 	c: mark => mark.a + mark.b,
@@ -175,10 +176,12 @@
 /// Visualise a mark's anatomy.
 ///
 /// #example(```
-/// let mark = fletcher.MARKS.stealth
-/// // make a wide stealth arrow
-/// mark += (angle: 45deg)
-/// fletcher.mark-debug(mark)
+/// context {
+/// 	let mark = fletcher.MARKS.get().stealth
+/// 	// make a wide stealth arrow
+/// 	mark += (angle: 45deg)
+/// 	fletcher.mark-debug(mark)
+/// }
 /// ```)
 ///
 /// - Green/left stroke: the edge's stroke when the mark is at the tip.
@@ -209,9 +212,9 @@
 	show-labels: true,
 	show-offsets: true,
 	offset-range: 6,
-) = {
-	mark = resolve-mark(mark)
-	stroke = as-stroke(stroke)
+) = context {
+	let mark = resolve-mark(mark)
+	let stroke = as-stroke(stroke)
 
 	let t = stroke.thickness
 	let scale = float(mark.scale)
@@ -320,9 +323,9 @@
 	stroke: 2pt,
 	width: 3cm,
 	height: 1cm,
-) = {
-	mark = resolve-mark(mark)
-	stroke = as-stroke(stroke)
+) = context {
+	let mark = resolve-mark(mark)
+	let stroke = as-stroke(stroke)
 
 	let t = stroke.thickness*float(mark.scale)
 

tests/mark-cap-offsets/test.typ
@@ -3,7 +3,7 @@
 #import "/src/marks.typ": *
 
 
-#align(center, MARKS.values().map(mark => {
+#context align(center, MARKS.get().values().map(mark => {
 	let e = 2.5
 	diagram(edge(
 		(0,0), (1,0),

tests/mark-debug/test.typ
@@ -1,7 +1,8 @@
 #set page(width: auto, height: auto, margin: 1em)
 #import "/src/exports.typ" as fletcher
 
-#fletcher.MARKS.pairs().map(((key, mark)) => [
+
+#context fletcher.MARKS.get().pairs().map(((key, mark)) => [
 	#raw(key)
 	#set align(center)
 	#fletcher.mark-debug(mark, show-offsets: true)

commit 666d7fb0cb3748fd745f5d6631c0fc30b12d6268

fix: regression cause by 13b452be

src/edge.typ
@@ -702,7 +702,6 @@
 	)
 
 	options += interpret-edge-args(args, options)
-	// options += interpret-marks-arg(options.marks)
 
 	// relative coordinate shorthands
 	let interpret-coord-str(coord) = {
@@ -722,12 +721,6 @@
 	options.vertices = options.vertices.map(interpret-coord-str)
 
 
-	options.stroke = if options.stroke != none {
-		(
-			cap: "round",
-			dash: options.dash,
-		) + stroke-to-dict(map-auto(options.stroke, (:)))
-	}
 
 	if options.label-side not in (left, center, right, auto) {
 		panic("`label-side` must be one of `left`, `center`, `right`, or `auto`; got " + repr(options.label-side))
@@ -764,8 +757,6 @@
 
 	edge += interpret-marks-arg(edge.marks)
 
-	edge.stroke = pass-none(stroke)(edge.stroke)
-
 	if edge.stroke == none {
 		// hack: for no stroke, it's easier to do the following.
 		// then we have the guarantee that edge.stroke is actually
@@ -776,9 +767,13 @@
 	}
 
 	edge.stroke = (
-		(thickness: 0.048em) + // guarantees thickness is a length
+		(
+			cap: "round",
+			dash: edge.dash,
+			thickness: 0.048em, // guarantees thickness is a length, not auto
+		) +
 		stroke-to-dict(options.edge-stroke) +
-		stroke-to-dict(edge.stroke)
+		stroke-to-dict(map-auto(edge.stroke, (:)))
 	)
 	edge.stroke.thickness = to-pt(edge.stroke.thickness)
 